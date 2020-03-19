54cb07587e0344e4a8d12996be142811247d8e9b
Robert Phillips <rpdecks@gmail.com>
2020-03-19 17:33:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,57 @@
-# My Code here....
+require 'pry'
+
+def map_to_negativize(source_array)
+  neg_array = []
+  i = 0 
+  while i < source_array.length do 
+    neg_array[i] = source_array[i] * -1
+    i += 1 
+  end
+  neg_array
+end
+
+def map_to_no_change(source_array)
+  new_array = source_array
+  new_array
+end
+
+def map_to_double(source_array)
+  double_array = []
+  i = 0 
+  while i < source_array.length do 
+    double_array[i] = source_array[i] * 2
+    i += 1 
+  end
+  double_array
+end
+
+def map_to_square(source_array)
+  square_array = []
+  i = 0 
+  while i < source_array.length do 
+    square_array[i] = source_array[i] **2
+    i += 1 
+  end
+  square_array
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  total = starting_point
+  i = 0 
+  while i < source_array.length do 
+    total = total += source_array[i]
+    i += 1
+  end
+  total
+end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do 
+    if !!source_array[i] = false
+
+end
+
+def reduce_to_any_true(source_array)
+  
+end
